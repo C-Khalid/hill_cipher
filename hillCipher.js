bb027d5140d8447fe60af197cd90bd65955f6f80
Khalid Alsuwaiyel <mekdif@gmail.com>
2014-12-27 16:09:41 -0500

    Create main tabs

--- hillCipher.js
+++ hillCipher.js
@@ -1,3 +1,13 @@
-(function() {
-  var app = angular.module('hillCipher', []);
-})();
+var app = angular.module('hillCipher',[]);
+ 
+app.controller('TabController', function(){
+  this.tab = 1;
+
+  this.setTab = function(tab){
+    this.tab = tab;
+  };
+
+  this.isSet = function(tab){
+    return this.tab === tab;
+  };
+});
