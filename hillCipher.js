856f65b1538aaf2f763cace08bc302225b8b8b8a
Khalid Alsuwaiyel <mekdif@gmail.com>
2014-12-28 04:37:25 -0500

    Add matrix key input

--- hillCipher.js
+++ hillCipher.js
@@ -11,3 +11,23 @@ app.controller('TabController', function(){
     return this.tab === tab;
   };
 });
+
+app.controller('MatrixController', function(){
+
+  //Default key length 2 X 2
+  this.keyLength = 2;
+
+  this.key = [];
+
+  //Populate the key array with noting to initialize it
+  this.initializeKeyArray = function(){
+	for(var i=0; i < this.keyLength; i++)
+	{
+	  this.key.push([]);
+	  for(var j=0; j < this.keyLength; j++)
+		this.key[i].push('');
+	}
+  };
+  this.initializeKeyArray();
+
+});
